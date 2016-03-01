85f5c20b1aea4802569a1d83642de4128a6c1688
Student <Student@Mac-mini-3.local>
2016-02-29 18:14:22 -0700

    started prompt and alert message

---
+++ main.js
@@ -0,0 +1,7 @@
+var word = prompt("Enter any word:")
+var msg1 = word + ", what a strange word to enter..\n Let's play around with it."
+var msg2 = word.charAt(0)
+var msg3 = word.toLowerCase()
+var msg4 = word.toUpperCase()
+var msg5 = word.substring(0,2)
+alert(msg1 + "\n" + msg2 + "\n" + msg3 + "\n" + msg4 + "\n" + msg5 + "\n")
